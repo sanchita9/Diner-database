1d4d3f89bca5b8f30d0bab809e94a1ffbd173e91
Hilton Adolinama <hiltonadolinama@Hiltons-MacBook-Pro.local>
2017-12-18 20:01:58 -0700

    Created server.js for routing

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app     = express();
+const path    = require("path");
+const PORT    = process.env.PORT || 3001;
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+// Send every request to the React app
+
+// ROUTES
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.get('/cheflogin', function(req, res) {
+    res.sendFile(path.join(__dirname + '/.client/build/chef.html'));
+});
+
+app.get('/dinerlogin', function(req, res) {
+    res.sendFile(path.join(__dirname + '/.client/build/diner.html'));
+
+app.get('/menu', function(req, res) {
+        res.sendFile(path.join(__dirname + '/.client/build/menu.html'));
+});
+
+app.listen(PORT, function() {
+  console.log(`🌎 ==> Server now on port ${PORT}!`);
+});
+
