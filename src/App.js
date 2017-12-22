1c80bc0e5b948b2b81eb61c1b8765c8814b45432
Hilton Adolinama <hiltonadolinama@Hiltons-MacBook-Pro.local>
2017-12-21 19:59:29 -0700

    Menu Route added

--- src/App.js
+++ src/App.js
@@ -10,12 +10,14 @@ import logo from './logo.svg';
 import './App.css';
 import Chef from './chef.js';
 import Diner from './diner.js';
+import Menu from './menu.js';
 
 const BasicExample = () => (
   <Router>
     <div>
       <Route path="/chef" component={Chef}/>
       <Route path="/diner" component={Diner}/>
+      <Route path="/menu" component={Menu}/>
     </div>
   </Router>
 )
