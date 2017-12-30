867b82ccdf50f1ba4724bc4a4ba209a6d70a1a8c
sanchita9 <sanchita9@gmail.com>
2017-12-29 22:22:19 -0700

    Added changes to appPost

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const cors     = require('cors');
 const mongoose = require('mongoose');
 const path     = require("path");
 
+const DB = require('./schema');
+
 app.use(cors());
 
 // ROUTES
@@ -27,11 +29,45 @@ app.get('/menu', (req, res) => {
   });
 
   app.post('/newChef', (req, res) => {
+    const chef = new DB.Chef({
+      username: req.body.username,
+      password: req.body.password,
+      photoUrl: req.body.photoUrl,
+      phoneNumber: req.body.phoneNumber,
+      emails: req.body.emails,
+      skills: req.body.skills
+    });
+
+    chef.save();
     res.json({"name": "newChef data"})
   });
 
   app.post('/newDiner', (req, res) => {
+    const diner = new DB.Diner({
+      username: req.body.username,
+      password: req.body.password,
+      name: req.body.name,
+      phoneNumber: req.body.phoneNumber,
+      emails: req.body.emails,
+      address: req.body.address,
+      order: req.body.order,
+      payment: req.body.payment
+    });
+
+    diner.save();
     res.json({"name": "newDiner data"})
+  });
+
+  app.post('/newMenu', (req, res) => {
+    const diner = new DB.Menu({
+      name: req.body.name,
+      price: req.body.price,
+      descriptions: req.body.descriptions,
+      photoUrl: req.body.photoUrl
+    });
+
+    menu.save();
+    res.json({"name": "newMenu data"})
   });
 
 app.listen(3005, () => console.log('Server app listening on port 3005!'));
