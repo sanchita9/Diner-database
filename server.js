3059c0b4ffdbe276795b6439f4654c9f07c50107
Hilton Adolinama <hiltonadolinama@Hiltons-MacBook-Pro.local>
2017-12-30 10:16:27 -0700

    Server.js editions

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 // DEPENDENCIES: express-cors-mongoose
-const express  = require('express');
-const app      = express();
-const cors     = require('cors');
+const express = require('express');
+const app = express();
+const cors = require('cors');
 const mongoose = require('mongoose');
-const path     = require("path");
+const path = require("path");
+
+// Import all models and save them to the name DB
+// This will allow you to access the model with dot notation 
+// for example: DB.Chef will refer to the Chef model you created in schema.js
+const DB = require('./schema');
+
+
+const DB = require('./schema');
 
 app.use(cors());
 
@@ -11,29 +19,83 @@ app.use(cors());
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.get('/home', (req, res) => {
-  res.json({"name": "Home Page"});
+  res.json({ "name": "Home Page" });
 });
 
 app.get('/diner', (req, res) => {
-    res.json({"name": "Chef Information"});
-  });
+  res.json({ "name": "Chef Information" });
+});
 
-app.get('/chef', (req, res) => {
-    res.json({"name": "Diner Information"});
+// using the ":" allows you to create a variable on the req.params object
+// in the example below we have created a key named "id" on the req.params object
+// when the front end makes a get request to this route they should supply an id
+// for example: /chef/123456 will have req.params.id === 123456
+app.get('/chef/:id', (req, res) => {
+  DB.Chef.findById(req.params.id).then(chef => {
+    res.send(chef);
   });
+});
 
 app.get('/menu', (req, res) => {
-    res.json({"name": "Menu Information"});
+  res.json({ "name": "Menu Information" });
+});
+
+  app.post('/newChef', (req, res) => {
+    const chef = new DB.Chef({
+      username: req.body.username,
+      password: req.body.password,
+      photoUrl: req.body.photoUrl,
+      phoneNumber: req.body.phoneNumber,
+      emails: req.body.emails,
+      skills: req.body.skills
+    });
+
+    chef.save();
+    res.json({"name": "newChef data"})
   });
+
+  app.post('/newDiner', (req, res) => {
+    const diner = new DB.Diner({
+      username: req.body.username,
+      password: req.body.password,
+      name: req.body.name,
+      phoneNumber: req.body.phoneNumber,
+      emails: req.body.emails,
+      address: req.body.address,
+      order: req.body.order,
+      payment: req.body.payment
+    });
+
+    diner.save();
+    res.json({"name": "newDiner data"})
 
 // POST ROUTES
 app.post('/newDiner', (req, res) => {
-    res.json({"name": "newDiner data"});
-  });
+  res.json({ "name": "newDiner data" });
+});
 
 app.post('/newChef', (req, res) => {
-    res.json({"name": "newChef data"});
-  });
+  const chef = new DB.Chef({
+    username: req.body.username,
+    password: req.body.password
 
+  });
+  // WARNING NOT COMPLETE
+
+  chef.save();
+  res.json({ "name": "newChef data" });
+});
+
+  app.post('/newMenu', (req, res) => {
+    const diner = new DB.Menu({
+      name: req.body.name,
+      price: req.body.price,
+      descriptions: req.body.descriptions,
+      photoUrl: req.body.photoUrl
+    });
+
+    menu.save();
+    res.json({"name": "newMenu data"})
+  });
 
 app.listen(3005, () => console.log('Server app listening on port 3005!'));
